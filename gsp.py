dafff324e639f3a435395729b740d64c7ff8ff90
Ian Thompson <ianiat11@gmail.com>
2022-04-11 22:33:49 -0500

    created dev branch

--- gsp.py
+++ gsp.py
@@ -30,6 +30,8 @@ def get_init_support(D):
 def generate_seed_set(prev_freq_seq, min_support=2):
     """"""
     
+    assert min_support > 0
+    
     _ = {k: (min_support - 1) for k in prev_freq_seq.keys()}  # subtract 1 from `min_support` to keep min frequent items
     
     _ = Counter(_)
@@ -39,6 +41,27 @@ def generate_seed_set(prev_freq_seq, min_support=2):
     seed_set = list(_.keys())
     
     return seed_set
+
+
+def join_phase(L_km1):
+    """"""
+    
+    L_km1
+
+
+#def is_subsequence():
+    #""""""
+    #...
+    
+
+
+#def generate_candidates(k):
+    #""""""
+    #L_km1  # the set of all frequent (k-1)-sequences -> use to generate a superset of the set of all frequent k-sequences (L_k)
+    #L_k  # the set of all frequent k-sequences
+    
+    #return C_k  # the set of candidate k-sequences
+    
     
 
 init_support = get_init_support(D)
